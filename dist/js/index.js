9d57b52cd29cb45d3c62490efd08677054d512bd
kevin <18288752086@139.com>
2017-04-10 16:05:29 +0800

    Initial commit

---
+++ dist/js/index.js
@@ -0,0 +1,69 @@
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
+
+var iconpicList=document.querySelectorAll('.link-content .iconpic');
+
+for (var i = 0; i < iconpicList.length; i++) {
+    iconpicList[i].style.background='url(images/link_content_'+ i +'.png)no-repeat center';
+}
+
+var poorList=document.querySelectorAll('.measure-poorlist ul li');
+
+for (var i = 0; i < poorList.length; i++) {
+    var icon=poorList[i].querySelector('.icon');
+    var title=poorList[i].querySelector('.title');
+    var line=poorList[i].querySelector('.line');
+    var liWidth=poorList[i].offsetWidth;
+    var iconWidth=icon.offsetWidth;
+    var titleWidth=title.offsetWidth;
+    var lineWidth=liWidth-iconWidth-titleWidth;
+    line.style.width=lineWidth+'px';
+}
+
+window.onresize=function(){
+    for (var i = 0; i < poorList.length; i++) {
+        var icon=poorList[i].querySelector('.icon');
+        var title=poorList[i].querySelector('.title');
+        var line=poorList[i].querySelector('.line');
+        var liWidth=poorList[i].offsetWidth;
+        var iconWidth=icon.offsetWidth;
+        var titleWidth=title.offsetWidth;
+        var lineWidth=liWidth-iconWidth-titleWidth;
+        line.style.width=lineWidth+'px';
+    }
+}
+
+var tabsNav=document.querySelectorAll('.tabsbox .tabsnav>div');
+var tabscontent=document.querySelectorAll('.tabsbox .tabscontent>ul')
+
+for (var i = 0; i < tabsNav.length; i++) {
+    tabsNav[i].index=i;
+    tabsNav[i].onclick=function(){
+        for (var j = 0; j < tabsNav.length; j++) {
+            tabsNav[j].className='';
+            tabscontent[j].className='';
+        }
+        this.className='current';
+        tabscontent[this.index].className='show';
+    }
+}
+
+var backBtn=document.querySelector('.topnav .back');
+
+backBtn.onclick=function(){
+    window.history.back();
+}
+
+var familyPersonList=document.querySelectorAll('.familyperson .listcontent>ul>li');
+
+for (var i = 0; i < familyPersonList.length; i++) {
+    familyPersonList[i].onclick=function(){
+        window.location.href='familyinfo.html'
+    }
+}
+
+
+
